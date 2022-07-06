20708f4c22886a7a1287f8dca1ac189321ee36c2
sidarth16 <ssidarth1999@gmail.com>
2022-07-06 07:48:47 +0530

    Success scenario

--- tests/selfie/test_selfie_challenge.py
+++ tests/selfie/test_selfie_challenge.py
@@ -1,7 +1,7 @@
 import pytest
 import brownie
 from brownie import DamnValuableTokenSnapshot, SimpleGovernance, SelfiePool
-from brownie import AttackRewarder
+from brownie import AttackSelfiePool
 from brownie import accounts , Wei , exceptions, chain
 
 deployer, attacker = None, None
@@ -28,37 +28,19 @@ def contracts():
 
     assert token.balanceOf(pool.address) == TOKENS_IN_POOL
 
-    contracts['token'] = tokens
+    contracts['token'] = token
     contracts["governance"] = governance
     contracts["pool"] = pool
 
     yield contracts
 
-#     #---  [ SUCCESS CONDITIONS ]  ---#
-    
-#     # Only one round should have taken place
-#     assert rewarderPool.roundNumber() == '3'
+    #---  [ SUCCESS CONDITIONS ]  ---#
 
-#      # Users should get neglegible rewards this round
-#     for user in users:
-#         rewarderPool.distributeRewards({"from":user})
-#         rewards = rewardToken.balanceOf(user.address)
-        
-#     # The difference between current and previous rewards balance should be lower than 0.01 tokens
-#     delta = rewards - Wei('25 ether')
-#     assert  delta < Wei(1e16)
-        
-#     # Rewards must have been issued to the attacker account
-#     assert rewardToken.totalSupply() > Wei('100 ether')
-#     rewards = rewardToken.balanceOf(attacker.address)
-        
-#     # The amount of rewards earned should be really close to 100 tokens
-#     delta = Wei('100 ether') - (rewards)
-#     assert delta < Wei(1e17)
-    
-#     # Attacker finishes with zero DVT tokens in balance
-#     assert liquidityToken.balanceOf(attacker.address) == ('0')
+    # Attacker has taken all tokens from the pool
+    assert token.balanceOf(attacker.address) == TOKENS_IN_POOL
 
+    assert token.balanceOf(pool.address) == '0'
+    
 
 def test_selfie_challenge(contracts):
     #-- [ CODE YOUR EXPLOIT HERE ] ---#
